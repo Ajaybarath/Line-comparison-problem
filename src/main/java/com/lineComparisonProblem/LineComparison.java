294f8861d27c40e6c8674df14538a5141ad0a81d
ajaybarath <ajaybarath3@gmail.com>
2021-08-20 17:40:14 +0530

    Line length comparator added

--- src/main/java/com/lineComparisonProblem/LineComparison.java
+++ src/main/java/com/lineComparisonProblem/LineComparison.java
@@ -4,15 +4,21 @@ public class LineComparison {
 	
 	public static void main(String args[]) {
 
-		Line line1 = new Line(2,4,4,5);
+		Line line1 = new Line(2,4,8,5);
 		Line line2 = new Line(4,4,6,5);
 		
-		if (line1.length().equals(line2.length())) {
-			System.out.println("The lines are equal");
+		int lineDifference = line1.length().compareTo(line2.length());
+		
+		if (lineDifference == 0) {
+			System.out.println("Both the lines are equal");
+		}
+		else if(lineDifference > 0){
+			System.out.println("Line 1 is greater by " + lineDifference);
 		}
 		else {
-			System.out.println("Lines are not equal");
+			System.out.println("Line 2 is greater by " + Math.abs(lineDifference));
 		}
+		
 		
 	}
 
