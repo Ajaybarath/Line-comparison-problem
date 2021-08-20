cbb24fc5f2a9563beabfecde95284cef543a2641
ajaybarath <ajaybarath3@gmail.com>
2021-08-20 17:00:20 +0530

    initial commit

---
+++ src/main/java/com/lineComparisonProblem/App.java
@@ -0,0 +1,13 @@
+package com.lineComparisonProblem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
